f7da90251a90c9f73be8c7424ca101b1d669f7ef
FatmaMuhammedAbdElHamid <fatmaabdelham@gmail.com>
2025-08-04 13:33:51 +0300

    Add files via upload

--- class_Ib.R
+++ class_Ib.R
@@ -1,22 +1,45 @@
+getwd()
+
+# creat project folder
 dir.create("raw_data")
 dir.create("clean_data")
 dir.create("scripts")
 dir.create("results")
 dir.create("plots")
+
+# import cvs file
 data <- read.csv(file.choose())
+
+# view data
 View(data)
+
+# check stucture
 str(data)
-data$gender_fac <- as.factor(data$gender)
+
+# convert gender to factor
+data$gender_factor <- as.factor(data$gender)
 str(data)
-data$gender_num <- ifelse(data$gender_fac == "Female",1,0)
+
+# convert factor to numric factor:
+# convert factor to numric
+data$gender_num <- ifelse(data$gender_factor == "Female",1,0)
 class(data$gender_num)
+# convert numric gender code to factor
 data$gender_num <- as.factor(data$gender_num)
 class(data$gender_num)
-data$diagnosis_fec <- as.factor(data$diagnosis)
+
+# convert diagnosis to factor
+data$diagnosis_fac <- as.factor(data$diagnosis)
 str(data)
-data$diagnosis_fec <- factor(data$diagnosis_fec,levels = c("Normal","Cancer"))
-levels(data$diagnosis_fec)
+
+# set factor level order manually 
+data$diagnosis_fac <- factor(data$diagnosis_fac, levels = c("Normal","Cancer"))
+levels(data$diagnosis_fac)
+str(data)
+
+# convert smoker to factor
 data$smoker_fac <- as.factor(data$smoker)
 str(data)
-data$smoker_num <- ifelse(data$smoker_fac == "No",0,1)
-write.csv(data, file = "patient_info_clean.csv", row.names = FALSE)
+
+# Create a new variable for smoking: 1 for "Yes", 0 for "No"
+data$smoker_num <- ifelse(data$smoker_fac == "No",0,1 )
